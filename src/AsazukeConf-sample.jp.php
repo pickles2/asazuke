d4ffc428f9733c49f4e542b58d023743a794db37
Misaki Shibata <misaki.pink@gmail.com>
2016-05-13 08:54:02 +0900

    uaにIMJ Agent 1.0を追加, mailtoの他のhrefパターンを追加

--- src/AsazukeConf-sample.jp.php
+++ src/AsazukeConf-sample.jp.php
@@ -19,8 +19,10 @@ class AsazukeConf extends AsazukeConfGeneral
     public static $projectName = 'sample.jp';
     
     // UserAgent
-    public static $userAgent = 'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; Trident/6.0)';
-    // public static $userAgent = 'Mozilla/5.0 (iPhone; CPU iPhone OS 8_0_2 like Mac OS X) AppleWebKit/600.1.4 (KHTML, like Gecko) Version/8.0 Mobile/12A405 Safari/600.1.4';
+    public static $userAgent = 'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; Trident/6.0)(IMJ Agent 1.0)';
+    // public static $userAgent = 'Mozilla/5.0 (iPhone; CPU iPhone OS 8_0_2 like Mac OS X) AppleWebKit/600.1.4 (KHTML, like Gecko) Version/8.0 Mobile/12A405 Safari/600.1.4(IMJ Agent 1.0)';
+    // public static $userAgent = 'Mozilla/5.0 (iPhone; CPU iPhone OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A403 Safari/8536.25 (IMJ Agent 1.0)';
+    // public static $userAgent = 'Mozilla/5.0 (Windows NT 6.3; WOW64; Trident/7.0; Touch; rv:11.0) like Gecko (IMJ Agent 1.0)';
     
 
     public static $csv_cols = [
